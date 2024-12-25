d1f001245233b6e5b13a7386ea8087e9abb3e0d0
Barcklan <160912116+Barcklan@users.noreply.github.com>
2024-12-24 21:13:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("MicrosoftOffice");
+        habilidades[1].classList.add("lenguajeR");
+        habilidades[2].classList.add("python");
+        habilidades[3].classList.add("SQL");
+        habilidades[4].classList.add("Vdatos");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+// Función para mostrar el modal
+function mostrarModal(texto) {
+    const modal = document.getElementById('modal');
+    const modalTexto = document.getElementById('modal-texto');
+    modalTexto.textContent = texto; // Agregar el texto dinámico
+    modal.style.display = 'block';
+}
+
+// Función para cerrar el modal
+function cerrarModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+function mostrarTooltip(event, texto) {
+    // Cerrar otros tooltips abiertos
+    document.querySelectorAll('.interes.active').forEach((interes) => {
+      interes.classList.remove('active');
+    });
+  
+    // Mostrar el tooltip del botón actual
+    const interes = event.currentTarget;
+    interes.classList.toggle('active');
+}
+
+// Cerrar el modal al hacer clic fuera de él
+window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    emailjs.init('PN2IV9-sfadPy25uK'); // Reemplaza 'YOUR_USER_ID' con tu User ID de EmailJS
+
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const serviceID = 'service_6gbjddk';
+        const templateID = 'template_vatdvvl';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(function() {
+                alert('¡Mensaje enviado!. ¡Muchas Gracias, Saludos!');
+            }, function(error) {
+                alert('Error al enviar el mensaje: ' + JSON.stringify(error));
+            });
+    });
+});
